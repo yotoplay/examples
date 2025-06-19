74af4ec3ba7b49bb794e47aeaad59392331634e2
Greg Sochanik <greg@yotoplay.com>
2025-06-19 21:11:28 +0100

    chore: fix a reference and add a fixed port so we can add to the list of calid callbacks

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,13 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Index from "./Index";
 import Login from "./pages/Login";
 import "./index.css";
-import App from "./pages/App";
+import AppForm from "./pages/AppForm";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/app" element={<App />} />
+      <Route path="/app" element={<AppForm />} />
       <Route path="/" element={<Index />} />
     </Routes>
   </BrowserRouter>
